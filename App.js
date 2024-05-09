0573c2026234f896a8b5461a956e85295465a37e
ammarAlaliS <ammaralialiwa@gmail.com>
2024-05-08 22:36:45 -0600

    adding  Navigation container dependency

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className=' text-xl text-white'>Tailwing css</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View className="flex-1 items-center justify-center bag-black">
+        <Text className=' text-6xl text-gray-200'>Tailwing  </Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
