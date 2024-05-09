934ad3014f184cff214078b14278dcf3025d8ab6
ammarAlaliS <ammaralialiwa@gmail.com>
2024-05-09 00:34:44 -0600

    add heroicon to the proyect

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,14 @@
 import { useNavigation } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React, { useLayoutEffect } from 'react';
-import { View, Text, SafeAreaView, Image} from 'react-native';
+import { View, Text, SafeAreaView, Image } from 'react-native';
 import GlobalStyles from '../components/GlobalStyles';
+import {
+    UserIcon, 
+    ChevronDownIcon, 
+    SearchIcon, 
+    AdjustmentsIcon,
+} from 'react-native-heroicons/outline'
 
 
 
@@ -18,15 +24,25 @@ const HomeScreen = () => {
 
     return (
         <SafeAreaView style={GlobalStyles.androidSafeArea}>
-            <View>
+            <View className=" flex-row pb-3 items-center mx-4 space-x-2">
                 <Image
                     source={{
                         uri: 'https://links.papareact.com/wru'
                     }}
-                    className=" h-7 w-7 bg-gray-300"
-                >
-                </Image>
+                    className=" h-7 w-7 bg-gray-300 p-4 rounded-full"
+                    />
+
+                <View className="flex-1">
+                    <Text className="font-bold text-gray-400 text-xs"> Deliver Now!</Text>
+                    <Text className=" font-bold text-xl"> 
+                    Current Location
+                    <ChevronDownIcon size={20} color="#00CC88"/>
+                    </Text>
+                 
+                </View>
+                <UserIcon size={35} color="#00CC88"/>
             </View>
+         
         </SafeAreaView>
     );
 }
